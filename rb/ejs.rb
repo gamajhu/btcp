5e550c7eb69ae51fbd368b2aa7072f64ac0edeae
Mario <gamajhu@hotmail.com>
2016-06-28 22:09:54 -0500

    btcps

---
+++ rb/ejs.rb
@@ -0,0 +1,64 @@
+#Calcular  la suma de los cuadrados x^2 de los números entre 1 y n.
+=begin
+n = gets.chomp.to_i
+total = 0
+for suma in (1..n)
+  potencias = (suma)**2
+  total += potencias 
+end
+puts total
+
+#Realizar un algoritmoque lea n números y obtenersupromedio, el número menor y el número mayor.
+puts "ingrese cuantos numeros quiere promediar"
+n = gets.chomp.to_i
+acum = 0 
+max = 0
+min = Float::MAX 
+for i in 1..n
+	puts "Ingrese numero #{i}: "
+	num = gets.chomp.to_f
+	acum += num
+	if (num > max)
+		max = num
+	end
+	if (num < min)
+		min = num 
+	end
+end
+
+#Escribir un algoritmo que recibe un número n, y se retorne las tablas de multiplicar del 1 al 20 de dicho número.!
+prom = acum / n
+puts "Promedio: #{prom} - Min : #{min} - Max : #{max} "
+
+puts "Ingrese el numero a multiplicar"
+n = gets.chomp.to_i
+for multiplicacion in 1..20
+	puts multiplicacion * n
+end
+
+
+
+#Reto Números amigos!Un par de número son amigos cuando la suma de los divisores de un número es igual a la suma de los divisores de la suma. 
+
+puts "Ingrese un numero"
+n = gets.chomp
+ for i in 1...n
+ 	dividision = n % i == 0
+ 	puts division 
+ end
+=end
+
+ puts "Introduzca su edad"
+ edad = gets.chomp.to_s
+ puts "Introduzca su peso"
+ peso = gets.chomp.to_f
+ puts "Introduzca su estatura"
+ estatura = gets.chomp.to_f
+
+ while (edad < 45.to_s)
+ 	conversion = (estatura * 100)**2
+ end
+
+ puts conversion
+ 
+
